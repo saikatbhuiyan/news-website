0eb7262f4993a00f310c29c5b5b080bf9d0896e9
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-03 12:20:07 +0600

    add .gitignore

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Main(models.Model):
+  """Create main object"""
+  name = models.TextField()
+
+  def __str__(self):
+    return self.name
