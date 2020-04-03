9e073c7d8c3bc1535235314b5333a47d80de40fa
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-03 12:40:37 +0600

    config main app

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Main
 
 
-
-
+def home(request):
+  
+  return render(request, 'home.html')
