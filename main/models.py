4e15d0a8bdfaae62c3dc1c64d04253c0fed9184b
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-10 01:12:00 +0600

    update main model

--- main/models.py
+++ main/models.py
@@ -3,11 +3,14 @@ from django.db import models
 
 class Main(models.Model):
   """Create main object"""
-  name = models.TextField()
-  about = models.TextField()
-  fb = models.TextField()
-  yt = models.TextField()
-  tw = models.TextField()
+  name = models.CharField(max_length=255, default='-')
+  about = models.CharField(max_length=255, default='-')
+  fb = models.CharField(max_length=255, default='-')
+  yt = models.CharField(max_length=255, default='-')
+  tw = models.CharField(max_length=255, default='-')
+  set_name = models.CharField(max_length=255, default='-')
+  tell = models.CharField(max_length=255, default='-')
+  link = models.CharField(max_length=255, default='-')
 
   def __str__(self):
-    return self.name
+    return self.set_name + " | " + str(self.pk)
