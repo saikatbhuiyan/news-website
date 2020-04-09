74a7c9373ebd674c581993f9b51fb72f3a34af06
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-09 23:15:45 +0600

    add about field

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Main(models.Model):
   """Create main object"""
   name = models.TextField()
+  about = models.TextField()
 
   def __str__(self):
     return self.name
