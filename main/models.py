aa8b2433c933414700caf51d67ddfc30d5e44d39
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-09 23:58:12 +0600

    social

--- main/models.py
+++ main/models.py
@@ -5,6 +5,9 @@ class Main(models.Model):
   """Create main object"""
   name = models.TextField()
   about = models.TextField()
+  fb = models.TextField()
+  yt = models.TextField()
+  tw = models.TextField()
 
   def __str__(self):
     return self.name
