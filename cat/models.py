d1923488f350fdf2776cde28ad365f0e81611eac
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-17 18:12:55 +0600

    add add button

--- cat/models.py
+++ cat/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
   """Create Category object"""
   name = models.CharField(max_length=255, default='-')
+  count = models.IntegerField(default=0)
 
   def __str__(self):
     return self.name + " | " + str(self.pk)
--- main/views.py
+++ main/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Main
 from news.models import News
+from cat.models import Category
 
 def home(request):
   site = Main.objects.get(pk=1)
-  news = News.objects.all()
+  news = News.objects.all().order_by('-pk')
+  cat = Category.objects.all()
   # sitename = "MySite | Home"
-  return render(request, 'front/home.html', {'site': site, 'news': news})
+  return render(request, 'front/home.html', {'site': site, 'news': news, 'cat': cat})
   
 def about(request):
   # sitename = "MySite | About"
